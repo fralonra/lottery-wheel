687d22a90a57a5707e92e88c0476c511de51c17d
fralonra <fralonra@aliyun.com>
2020-05-03 21:30:46 +0800

    refactor: use svg.js to replace snapsvg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import anime from 'animejs'
-import Snap from 'snapsvg'
+import { SVG } from '@svgdotjs/svg.js'
+import '@svgdotjs/svg.filter.js'
 
 const baseFontSize = 16
 
@@ -51,7 +52,9 @@ class Wheel {
     if (!this.option.el) throw new Error('el is undefined')
     if (!this.option.data) throw new Error('data is undefined')
     const len = this.option.data.length
-    if (len < 3 || len > 12) { throw new Error('data.length must between 3 and 12') }
+    if (len < 3 || len > 12) {
+      throw new Error('data.length must between 3 and 12')
+    }
 
     this._count = 0
     this._rotation = 0
@@ -89,11 +92,10 @@ class Wheel {
   draw () {
     const opt = this.option
 
-    this._center = opt.pos.map((p) => p + opt.radius)
+    this._center = opt.pos.map(p => p + opt.radius)
 
-    const svg = Snap(opt.el)
-    svg.node.style.width = opt.radius * 2 + 'px'
-    svg.node.style.height = opt.radius * 2 + 'px'
+    const svg = SVG(opt.el)
+      .size(opt.radius * 2, opt.radius * 2)
 
     this._deg = 360 / opt.data.length
 
@@ -118,8 +120,8 @@ class Wheel {
       opt.inRadius = opt.radius
     }
 
-    this._drawTurntable(svg)
-    this._drawButton(svg)
+    if (!this._turntable) this._drawTurntable(svg)
+    if (!this._button) this._drawButton(svg)
   }
 
   _drawResource (svg) {
@@ -127,26 +129,22 @@ class Wheel {
     const res = opt.image
     if (typeof res === 'object' && Object.keys(res).length > 0) {
       if (res.turntable && typeof res.turntable === 'string') {
-        this._turntable = svg.image(
-          res.turntable,
-          opt.pos[0],
-          opt.pos[1],
-          opt.radius * 2,
-          opt.radius * 2
-        )
+        this._turntable = svg
+          .image(res.turntable)
+          .move(opt.pos[0], opt.pos[1])
+          .size(opt.radius * 2, opt.radius * 2)
       }
       if (res.button && typeof res.button === 'string') {
         if (!res.offset || typeof res.offset !== 'number') res.offset = 0
         const size = getImageSize(res.button)
         const buttonHeight = (size[1] * opt.buttonWidth) / size[0]
-
-        this._button = svg.image(
-          res.button,
-          this._center[0] - opt.buttonWidth / 2,
-          this._center[1] + res.offset - Math.round(buttonHeight / 2),
-          opt.buttonWidth,
-          buttonHeight
-        )
+        this._button = svg
+          .image(res.button)
+          .move(
+            this._center[0] - opt.buttonWidth / 2,
+            this._center[1] + res.offset - buttonHeight / 2
+          )
+          .size(opt.buttonWidth, buttonHeight)
       }
     }
   }
@@ -155,74 +153,63 @@ class Wheel {
     if (this._turntable) return
 
     const opt = this.option
-    let obj = svg.circle(this._center[0], this._center[1], opt.radius)
-    obj.attr({
-      fill: opt.color.border
-    })
-    obj = svg.circle(this._center[0], this._center[1], opt.inRadius)
-    obj.attr({
-      fill: opt.color.prize
-    })
+    svg
+      .circle(opt.radius * 2)
+      .center(this._center[0], this._center[1])
+      .fill(opt.color.border)
+    svg
+      .circle(opt.inRadius * 2)
+      .center(this._center[0], this._center[1])
+      .fill(opt.color.prize)
 
-    this._turntable = svg.g()
+    this._turntable = svg.group()
     for (const d of opt.data) {
-      const r = opt.inRadius
-
       const [pathD, dLen] = describeArc(
         this._center[0],
         this._center[1],
-        r,
+        opt.inRadius,
         -this._deg / 2,
         this._deg / 2
       )
-      const pie = svg.path(pathD)
-      pie.attr({
-        fill: d.color ? d.color : opt.color.prize,
-        stroke: opt.color.line,
-        strokeWidth: 2
-      })
 
-      let fontSize = d.fontSize
-        ? d.fontSize
-        : opt.fontSize
-          ? opt.fontSize
-          : baseFontSize
-      let textSum = 0
-      for (let i = 0; i < d.text.length; ++i) {
-        if (d.text[i].match(/\w/)) {
-          textSum += 1
-        } else textSum += 2
-      }
-      if (!opt.fontSize && !d.fontSize) {
+      const pie = svg
+        .path(pathD)
+        .fill(d.color || opt.color.prize)
+        .stroke({ color: opt.color.line, width: 2 })
+
+      let fontSize = d.fontSize || opt.fontSize
+      if (!fontSize) {
+        let textSum = 0
+        for (let i = 0; i < d.text.length; ++i) {
+          if (d.text[i].match(/\w/)) {
+            textSum += 1
+          } else textSum += 2
+        }
         fontSize =
-          (fontSize * textSum) / 2 > dLen * opt.textBottomPercentage
+          (baseFontSize * textSum) / 2 > dLen * opt.textBottomPercentage
             ? ((dLen * opt.textBottomPercentage) / textSum) * 2
-            : fontSize
+            : baseFontSize
       }
-      const text = svg.text(
-        this._center[0],
-        opt.pos[1] +
-          opt.radius -
-          r * opt.textBottomPercentage * Snap.cos(this._deg / 2) -
-          fontSize,
-        d.text
-      )
-      text.attr({
-        fill: d.fontColor ? d.fontColor : opt.color.prizeFont,
-        fontSize: opt.fontSize ? opt.fontSize : fontSize
-      })
-      const box = text.node.getBoundingClientRect()
-      text.transform(new Snap.Matrix().translate(-Math.round(box.width / 2), 2))
-
-      const g = svg
-        .g(pie, text)
-        .transform(
-          new Snap.Matrix().rotate(
-            this._deg * opt.data.indexOf(d),
-            this._center[0],
-            this._center[1]
-          )
+      const text = svg
+        .plain(d.text)
+        .move(
+          this._center[0],
+          opt.pos[1] +
+            opt.radius -
+            opt.inRadius * opt.textBottomPercentage - fontSize
         )
+        .fill(d.fontColor || opt.color.prizeFont)
+        .font({ size: fontSize })
+      text.translate(-text.length() / 2, 2)
+      const g = svg
+        .group()
+        .rotate(
+          this._deg * opt.data.indexOf(d),
+          this._center[0],
+          this._center[1]
+        )
+      g.add(pie)
+      g.add(text)
       this._turntable.add(g)
     }
   }
@@ -243,38 +230,51 @@ class Wheel {
       deg - 360,
       360 - deg
     )
-    const top = [center[0], center[1] - r / Snap.cos(deg)]
-    const pathD = `${pathArc}L${top[0]},${top[1]}L${end.x},${end.y}L${center[0]},${center[1]}`
-    const b = svg.path(pathD)
-    b.attr({
-      fill: opt.color.button,
-      filter: svg.filter(Snap.filter.shadow(0, 3, 3, 'black', 0.5))
-    })
+    const top = [center[0], center[1] - r / Math.cos(degreeToRadians(deg))]
+    const pathD = `${pathArc} L ${top[0]} ${top[1]} L ${end.x} ${end.y} L ${center[0]} ${center[1]}`
+    const button = svg.path(pathD)
+      .fill(opt.color.button)
+      .filterWith(function (add) {
+        add.$autoSetIn = false
+        add.gaussianBlur(3).in(add.$sourceAlpha)
+        add.offset(0, 3)
+        add.componentTransfer(function (add) {
+          add.funcA({
+            type: 'linear',
+            slope: 0.5
+          })
+        })
+        add.merge([null, add.$source])
+        add.attr('filterUnits', 'userSpaceOnUse')
+      })
 
     let text = null
     if (opt.buttonText !== '') {
       const maxLen = r * 2 * 0.8
-      let fontSize = opt.buttonFontSize ? opt.buttonFontSize : baseFontSize
-      let textSum = 0
-      for (let i = 0; i < opt.buttonText.length; ++i) {
-        if (opt.buttonText[i].match(/\w/)) {
-          textSum += 1
-        } else textSum += 2
-      }
-      if (!opt.buttonFontSize) {
+      let fontSize = opt.buttonFontSize
+      if (!fontSize) {
+        let textSum = 0
+        for (let i = 0; i < opt.buttonText.length; ++i) {
+          if (opt.buttonText[i].match(/\w/)) {
+            textSum += 1
+          } else textSum += 2
+        }
         fontSize =
-          (fontSize * textSum) / 2 > maxLen ? (maxLen / textSum) * 2 : fontSize
+          (baseFontSize * textSum) / 2 > maxLen
+            ? (maxLen / textSum) * 2
+            : baseFontSize
       }
-      text = svg.text(center[0], center[1], opt.buttonText)
-      text.attr({
-        fill: opt.color.buttonFont,
-        fontSize: opt.buttonFontSize ? opt.buttonFontSize : fontSize
-      })
-      const box = text.node.getBoundingClientRect()
-      text.transform(new Snap.Matrix().translate(-Math.round(box.width / 2), 2))
+      text = svg
+        .plain(opt.buttonText)
+        .move(center[0], center[1] - fontSize / 2)
+        .fill(opt.color.buttonFont)
+        .font({ size: fontSize })
+      text.translate(-text.length() / 2, 2)
     }
 
-    this._button = svg.g(b, text)
+    this._button = svg.group()
+    this._button.add(button)
+    this._button.add(text)
   }
 
   _animeFunc () {
@@ -284,26 +284,24 @@ class Wheel {
 
     this._button.node.style.cursor = 'pointer'
     this._button.node.style['transform-origin'] = 'center'
-    this._button.hover(
-      () => {
-        if (opt.onButtonHover && typeof opt.onButtonHover === 'function') {
-          opt.onButtonHover(anime, this._button)
-          return
-        }
-        anime({
-          targets: this._button.node,
-          scale: 1.2,
-          duration: 500
-        })
-      },
-      () => {
-        anime({
-          targets: this._button.node,
-          scale: 1,
-          duration: 500
-        })
+    this._button.mouseover(() => {
+      if (opt.onButtonHover && typeof opt.onButtonHover === 'function') {
+        opt.onButtonHover(anime, this._button)
+        return
       }
-    )
+      anime({
+        targets: this._button.node,
+        scale: 1.2,
+        duration: 500
+      })
+    })
+    this._button.mouseout(() => {
+      anime({
+        targets: this._button.node,
+        scale: 1,
+        duration: 500
+      })
+    })
     this._button.click(() => {
       this._run()
     })
@@ -318,7 +316,7 @@ class Wheel {
       return
     }
 
-    const runAnime = (pie) => {
+    const runAnime = pie => {
       if (this._rotation > 0) {
         const revision = 360 - (this._rotation % 360)
         this._rotation += revision
@@ -366,8 +364,12 @@ function getInnerRadius (radius) {
   return Math.round(radius / 10) * 9
 }
 
+function degreeToRadians (degree) {
+  return degree * Math.PI / 180
+}
+
 function polarToCartesian (centerX, centerY, radius, angleInDegrees) {
-  const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180
+  const angleInRadians = degreeToRadians(angleInDegrees - 90)
   return {
     x: centerX + radius * Math.cos(angleInRadians),
     y: centerY + radius * Math.sin(angleInRadians)
@@ -379,26 +381,7 @@ function describeArc (x, y, radius, startAngle, endAngle) {
   const end = polarToCartesian(x, y, radius, startAngle)
 
   const largeArcFlag = endAngle - startAngle <= 180 ? 0 : 1
-
-  const d = [
-    'M',
-    start.x,
-    start.y,
-    'A',
-    radius,
-    radius,
-    0,
-    largeArcFlag,
-    0,
-    end.x,
-    end.y,
-    'L',
-    x,
-    y,
-    'L',
-    start.x,
-    start.y
-  ].join(' ')
+  const d = `M ${start.x} ${start.y} A ${radius} ${radius} 0 ${largeArcFlag} 0 ${end.x} ${end.y} L ${x} ${y} L ${start.x} ${start.y}`
   const l = start.x - end.x
   return [d, l, start, end]
 }
