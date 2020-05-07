89f1956b9d1b8dc373a46c27fc0a503d2540ca69
fralonra <zoronlivingston@gmail.com>
2020-05-07 11:00:59 +0800

    ci: build with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import { terser } from 'rollup-plugin-terser'
+import babel from '@rollup/plugin-babel'
 import filesize from 'rollup-plugin-filesize'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+
+const babelOption = {
+  presets: [['@babel/env', { modules: false }]]
+}
 
 export default [{
   input: 'src/index.js',
   plugins: [
     commonjs(),
     resolve(),
+    babel(babelOption),
     filesize()
   ],
   output: {
@@ -20,6 +26,7 @@ export default [{
   plugins: [
     commonjs(),
     resolve(),
+    babel(babelOption),
     terser(),
     filesize()
   ],
